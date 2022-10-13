729c603d92cd7c9cb808479e7cd3682a6f50ac4e
llebielle <lucas@lebielle.com>
2022-10-12 23:34:02 -0400

    Are you sure page

--- src/PageAskSolo.jsx
+++ src/PageAskSolo.jsx
@@ -1,44 +1,60 @@
-import { Component } from "react";
+import {Component} from "react";
 import "./styles.css";
+import "./css/PageAskSolo.css";
 
 import traduction from "./traduction";
 import idPage from "./idPage";
 
 class PageAskSolo extends Component {
-  render() {
-    return (
-      <div className="mainTab">
+    render() {
+        return (
+            <div className="mainTab">
 
+                <h2>{traduction[this.props.language]["DAY"]}</h2>
 
-        <div className="radioGroup">
-          <input
-              type="button"
-              className={ !this.props.soloPlay
-                  ? "active"
-                  : "inactive"
-              }
-              value={traduction[this.props.language]["COMPETITIVE"]}
-              onClick={() => this.props.goCompetitive()}
-          />
-        <input
-              type="button"
-              className={ this.props.soloPlay
-                  ? "active"
-                  : "inactive"
-              }
-              value={traduction[this.props.language]["SOLO"]}
-              onClick={() => this.props.goSolo()}
-          />
-      </div>
-        <input
-            className="fullgreen"
-            type="button"
-            value={traduction[this.props.language]["PLAY"]}
-            onClick={() => this.props.changePage(idPage["P_INGAME"])}
-        />
-      </div>
-    );
-  }
+                <div class="content">
+                    <p>{traduction[this.props.language]["GAMETYPE"]}</p>
+
+                    <div className="radioGroup">
+                        <input
+                            type="button"
+                            className={!this.props.soloPlay
+                                ? "active"
+                                : "inactive"
+                            }
+                            value={traduction[this.props.language]["COMPETITIVE"]}
+                            onClick={() => this.props.goCompetitive()}
+                        />
+                        <input
+                            type="button"
+                            className={this.props.soloPlay
+                                ? "active"
+                                : "inactive"
+                            }
+                            value={traduction[this.props.language]["SOLO"]}
+                            onClick={() => this.props.goSolo()}
+                        />
+                    </div>
+                    <input
+                        className="fullgreen"
+                        type="button"
+                        value={traduction[this.props.language]["PLAY"]}
+                        onClick={() => this.props.changePage(idPage["P_INGAME"])}
+                    />
+                </div>
+                <div className="footer">
+                    <a
+                        id="homeBut"
+                        className="backlink"
+                        type="submit"
+                        onClick={() => this.props.changePage(idPage["P_MAIN"])}
+                    >
+                        {traduction[this.props.language]["BACKHOME"]}
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default PageAskSolo;
