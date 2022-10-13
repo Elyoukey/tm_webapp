3b7f3d3b9eb51b9633844c7189e44630ff6333e3
llebielle <lucas@lebielle.com>
2022-10-12 23:23:33 -0400

    Are you sure page

--- src/PageAreYouSure.jsx
+++ src/PageAreYouSure.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import {Component} from "react";
 import "./styles.css";
 import "./css/PageAreYouSure.css";
 
@@ -6,32 +6,32 @@ import traduction from "./traduction";
 import idPage from "./idPage";
 
 class PageAreYouSure extends Component {
-  render() {
-    return (
-      <div className="mainTab">
-<div className={"message"}>
+    render() {
+        return (
+            <div className="mainTab">
+                <div className={"message"}>
 
 
-        <p>{traduction[this.props.language]["SHOW_SOLUTION"]}</p>
-        <div className="radioGroup small">
-            <input
-                type="button"
-                className="fullgreen"
-                value={traduction[this.props.language]["YES"]}
-                onClick={() => this.props.changePage(idPage["P_SOLUTION"])}
-            />
-            <input
-                type="button"
-                className="fullgreen"
-                value={traduction[this.props.language]["NO"]}
-                onClick={() => this.props.changePage(idPage["P_INGAME"])}
-            />
-        </div>
-</div>
+                    <p>{traduction[this.props.language]["SHOW_SOLUTION"]}</p>
+                    <div className="radioGroup small">
+                        <input
+                            type="button"
+                            className="fullgreen"
+                            value={traduction[this.props.language]["YES"]}
+                            onClick={() => this.props.changePage(idPage["P_SOLUTION"])}
+                        />
+                        <input
+                            type="button"
+                            className="fullgreen"
+                            value={traduction[this.props.language]["NO"]}
+                            onClick={() => this.props.changePage(idPage["P_INGAME"])}
+                        />
+                    </div>
+                </div>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default PageAreYouSure;
