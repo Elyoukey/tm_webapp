ee86c96c5a0b49f37ec577a7bcf808aff7cef116
llebielle <lucas@lebielle.com>
2022-10-20 22:22:07 -0400

    Fix code input

--- src/PageSearch.jsx
+++ src/PageSearch.jsx
@@ -1,31 +1,41 @@
-import { Component } from "react";
-import "./styles.css";
+import {Component} from "react";
+import "./css/PageSearch.css";
 
 import traduction from "./traduction";
 import idPage from "./idPage";
 
 class PageSearch extends Component {
-  render() {
-    return (
-      <div className="mainTab">
+    render() {
+        return (
+            <div className="mainTab pageSearch">
 
-        <h2>{traduction[this.props.language]["SEARCH"]}</h2>
-        <p>{traduction[this.props.language]["INPUTGAMECODE"]}</p>
+                <h2>{traduction[this.props.language]["SEARCH"]}</h2>
+                <p>{traduction[this.props.language]["INPUTGAMECODE"]}</p>
 
-<input
-    type="text"
-    className="bigInput"
-    onChange={(e) => this.props.handleChange(e.target.value)}
-/>
-          <input
-              type="button"
-              className="fullgreen"
-              value={traduction[this.props.language]["LOAD"]}
-              onClick={() => this.props.hashGame()}
-          />
-      </div>
-    );
-  }
+                <input
+                    type="text"
+                    className="bigInput"
+                    onChange={(e) => this.props.handleChange(e.target.value)}
+                />
+                <input
+                    type="button"
+                    className="fullgreen"
+                    value={traduction[this.props.language]["LOAD"]}
+                    onClick={() => this.props.hashGame()}
+                />
+                <div className="footer">
+                    <a
+                        id="homeBut"
+                        className="backlink"
+                        type="submit"
+                        onClick={() => this.props.changePage(idPage["P_MAIN"])}
+                    >
+                        {traduction[this.props.language]["BACKHOME"]}
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default PageSearch;
